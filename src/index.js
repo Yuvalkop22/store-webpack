2e342dc6d6041ce443a889811b8c611d75a2ebab
Yuvalkop22 <Kopelman050@gmail.com>
2022-12-26 22:35:52 +0200

    script tag removed from html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import {bro} from './bro';
 import "./styles/main.css";
 
+document.querySelector("button").addEventListener("click",result);
+function result() {
+    let a= document.getElementById('productName').value;
+    document.querySelector('.navBar').append(a);
+}
