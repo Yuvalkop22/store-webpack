2521960ca1673fcca8abd75e7496b55e69afc8c4
Yuvalkop22 <Kopelman050@gmail.com>
2022-12-27 11:21:19 +0200

    add to cart

---
+++ src/cart.js
@@ -0,0 +1,4 @@
+// import {selectedProducts} from '../src/catalog';
+// console.log(selectedProducts)
+//var str = selectedProducts[0].name;
+// document.querySelector(".cart").append(str);
---
+++ src/catalog.js
@@ -0,0 +1,23 @@
+import {products} from './products';
+import "./styles/main.css";
+let selectedProducts = [];
+for (let i = 0; i< products.length; i++){
+    var catalogDiv = document.createElement("div");
+    document.querySelector(".catalog").append(catalogDiv);
+    catalogDiv.setAttribute('style', 'border: 1px solid black; height: 120px; width: 20%');
+    var newH1 = document.createElement("h3");
+    newH1.innerHTML = products[i].name;
+    var newH2 = document.createElement("h3");
+    newH2.innerHTML = "price: " + products[i].price;
+    var btn = document.createElement("button");
+    btn.setAttribute('style', 'margin-bottom: 20px')
+    btn.innerHTML = "Add to cart";
+    catalogDiv.append(newH1);
+    catalogDiv.append(newH2);
+    catalogDiv.append(btn);
+    btn.addEventListener("click",function f() {
+        console.log(products[i])
+        selectedProducts.push(products[i]);
+    });
+}
+export {selectedProducts};
